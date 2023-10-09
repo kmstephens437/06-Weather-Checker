3ff0f7d99ce888adb1e5347728a8c76a883833da
Kevin-Stephens <kmstephens437@gmail.com>
2023-10-09 11:44:35 -0500

    setting variables and renaming ids

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,6 @@
+const city = document.querySelector("#city");
+const previousCitiesArea = document.querySelector("#recentSearches");
+const currentWeatherArea = document.querySelector("#current");
+const currentConditions = ("#weather")
+const fiveDayArea = document.querySelector("#fiveDay");
 
